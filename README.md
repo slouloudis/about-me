b6cf6f410144ffc12aaf96ee072fe3d4782520ce
slouloudis~ <samanthaleelouloudis@gmail.com>
2023-01-24 23:52:31 +0000

    lab 03

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # about-me
+
+let correctAnswers = 0
+
+// make a function called makeQuiz(ID, questions.OBJ)
+questions.OBJ - has question inside it. 
+
+
+make quiz will take two arguments - an empty div and some objects (questionsOBJ)
+
+      do a for loop that looks at our objects and see how many questions there are -
+      for each, create 4x elements on an empty div in my html with an ID. 
+
+      elements -
+      quiz input
+      quiz label
+      a submit button
+      a form - checks submission is correct
+
+
+      can use classList.add to add css classes.
+
+      can use .type to set out submit button to a button (use input when making)
+      use event.preventDefault() to stop the page from reloading on submit.
+
+      if the submission is correct use alert() (question[i].correct)
+        correctAnswers++
+
+append my quiz input, quiz label, and submit button to the form. 
+Append form element to root element (htmlID)
+
+//
+Call my function (ID,[
+The questions.OBJ has: 
+                {[Question:1, [A,few,answers], and correct:1]}
+                {[Question:2, [A,few,answers], and correct:2]}
+                {[Question:3, [A,few,answers], and correct:3]}
+                {[Question:4, [A,few,answers], and correct:4]}
+])
+
+https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+
+- Create a "Top Ten" at the bottom of your HTML page as an ordered list in HTML. Some ideas that you can include could be top ten movies, top ten favorite places, or top ten places to visit. You may choose to do whatever top ten list that you wish. DONE
+
+
+- Convert your work experience and education summary into an unordered list using HTML 
--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 // all questions must take y/n or yes/no. Use .toUpperCase
 
+let total = 0
 let userName = prompt("What is your name?")
 
 function question1() { 
 
-  let q1 = confirm("Did I go to space? y/n");
+  let q1 = confirm("Did I go to space? ╰( ͡° ͜ʖ ͡° )つ──☆*:・ﾟy/n");
 
   if (q1 == true) {
     console.log("Sick");
     // console.log(userName)
     document.getElementById('q1ID').innerHTML ="<p>I have not been to space, " + userName + "! </p>"
     // document.write("<p>I have not been to space!" + userName + "</p>")
+    total++
   } else{
     console.log("boo!");
     document.write("<p>One day we'll go, " + userName + "</p>")
@@ -25,10 +27,12 @@ function question2() {
     case "yes":
       console.log("Wow!")
       document.write("<p>I am great at tekken!**</p>")
+      total++
         break;
     case "y":
       console.log("Wow!!")
       document.write("<p>I am great at tekken!**</p>")
+      total++
         break;
     case "no":
       console.log("Wow!!!")
@@ -56,11 +60,13 @@ function question3() {
     case "no":
       console.log("Super!!")
       document.write("<p>Correct! It was in art</p>")
+      total++
         break;
     case "concept art":
     case "concept art/design":
       console.log("Super!!!")
       document.write("<p>It wasn't very effective!</p>")
+      total++
         break;
     // case  (q3==null?"":q3):
     case "":
@@ -77,22 +83,106 @@ function question4() {
 
   if (q4 == true) {
     console.log("Correct!")
-    document.write("<p>Yes! We moved to mossel bay when I was around 3 or 4 though.</p>")
+    document.write("<p>Yes! We moved to mossel bay when I was around 3 or 4 though.</p>");
+    total++
   } else {
     console.log("No!")
-    document.write("<p>Sadly, I wasn't born on Mars </p>")
+    document.write("<p>Sadly, I wasn't born on Mars </p>");
   }
 }
 
 function question5() {
-  let q5 = prompt("One day, I'm gonna become Hokage. Do you belive it?");
+  let q5 = prompt("One day, I'm gonna become Hokage. Do you belive it? (҂◡_◡) ᕤ");
   
   if (q5 === "y" || q5 === "yes") {
     console.log("hmm")
-    document.write("They'll carve my face on a mountain!")
+    document.write("They'll carve my face on a mountain!");
+    total++
   } else {
     console.log("noo")
-    document.write("Have some faith!")
+    document.write("Have some faith!");
   }
 }
-   
+
+// this changes everytime so good luck lol
+function question6() {
+  let numberGame = prompt("Guess a number! Between 1-10 ┌( ͝° ͜ʖ͡°)=ε/̵͇̿̿/’̿’̿ ̿");
+  let correctNumber = Math.floor(Math.random() * 11);
+  // correctNumber = 5
+
+  if (numberGame < correctNumber) {
+    alert("Too low!");
+    return false;
+  } else if (numberGame > correctNumber) {
+    alert("Too high!");
+    return false;
+  } else if (isNaN(numberGame)) {
+    alert("That isn't a number!")  
+  } else {
+    alert("That's correct!")
+    document.write("You guessed my number right! ┌( ͝° ͜ʖ͡°)=ε/̵͇̿̿/’̿’̿ ̿")
+    total++
+  }
+}
+
+// function question7() {
+//     //Add a 7th question that has multiple possible correct answers that are stored in an array.
+//     // Give the user 6 attempts to guess the correct answer.
+//     answers = ["red", "blue", "green","yellow", "brown", "orange","purple","pink","magenta","skyblue"]
+//     attempts = 6
+//     for (let i = 0; i < 6; i++){
+//       userAnswer = prompt("Guess one of the colours I'm thinking of! You have 6 attempts ☜(⌒▽⌒)☞")
+//       console.log(i)
+//       if (userAnswer.toLowerCase() == answers[0] || userAnswer.toLowerCase() == answers[5] || userAnswer.toLowerCase() == answers[8]) { 
+//           alert("Yep! " + userAnswer + " was one of the colours I was thinking of. Don't you hate that we have to spell colour 'color' in css?!?");
+//           document.write("You guessed my colour right! ✌(-‿-)✌")
+//           return false;
+//       } else {
+//         --attempts
+//         alert("Try again! You have " + attempts + " attempts left")
+//         console.log("attempt i number" + attempts)
+//         if (attempts == 0){
+//           document.write("You ran out of attempts! Reload the page to try again. PS (One of the possible answers was 'red'!")
+//         }
+//       }
+//     }
+// }
+
+function question7() {
+  // create array with multiple correct stored answers.
+  let answers = ["red", "orange", "magenta"]
+  let correct = false
+  let userAnswer = null
+  attempts = 6
+  // ask the user the q and then check if it is correct. 
+
+  for (let i = 0; i < 6; i++) {
+    userAnswer = prompt("Guess one of the colours I'm thinking of! You have " + attempts + " attempts ☜(⌒▽⌒)☞") 
+    for (let j = 0; j < answers.length; j++) {
+      if (userAnswer == answers[j]) {
+        console.log("true?!")
+        correct = true
+      }
+    }
+    if (correct == true) {
+      break;
+    }
+    --attempts
+    if (attempts == 0) {
+      alert("Sorry! That was your last guess. (╥﹏╥)")
+      break;
+    }
+    alert("Try again! You have " + attempts + " extra lives left")
+    console.log(attempts)
+  }
+  // give the user 6 attemps to get it correct. 
+  
+  if (correct == true) {
+    alert("Yep! " + userAnswer + " was one of the colours I was thinking of. Don't you hate that we have to spell colour 'color' in css?!?");
+    document.write("You guessed my colour right! ✌(-‿-)✌")
+    total++
+  } else {
+    document.write("You ran out of attempts! Reload the page to try again. PS (One of the possible answers was 'red'!")
+  }
+  alert("Ö You got " + total + " questions correct (๑•́ ヮ •̀๑) ")
+}
