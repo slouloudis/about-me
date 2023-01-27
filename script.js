554debd61df87ff494dfe3e91798fbf0f4865a13
slouloudis~ <samanthaleelouloudis@gmail.com>
2023-01-27 11:23:04 +0000

    quick push

---
+++ script.js
@@ -0,0 +1,28 @@
+let questions1 = [{
+  question: "Did I go to space? ╰( ͡° ͜ʖ ͡° )つ──☆*:・ﾟy/n",
+  answers: ["yes","no"],
+  corret: "no",
+},
+  question2 = {
+  question: "Am I great at Tekken? y/n",
+  answers: ["yes","no"],
+  corret: "yes",
+}]
+
+
+
+
+function makeQuiz(ID, greatQuestions) {
+  for (let i = 0; i <= greatQuestions.length; i++) {
+    //create input
+
+    //create label
+
+    //create submit
+
+    //create form -- check if correct. 
+
+    form.action = '#'
+  }
+  
+}
